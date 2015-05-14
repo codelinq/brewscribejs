40c76f542b6e2e1a961e082afa39b0d4deb91c8a
Scott Beardsley <scott@beards.ly>
2015-05-14 08:28:11 -0400

    modified tests

--- test/document_spec.js
+++ test/document_spec.js
@@ -1,8 +1,6 @@
 var should = require('should');
 var DocumentParser = require('./../lib/DocumentParser');
 
-var folder = process.env.HOME + '/documents/beersmith2';
-
 describe('document_parser', function(){
 	it('should spit out lines', function(done){
 		this.timeout(7000);
@@ -17,7 +15,7 @@ describe('document_parser', function(){
 		parser.on('end', function(){
 			done();
 		});
-		parser.parse(folder + '/Recipe.bsmx', 'Recipe');
+		parser.parse(__dirname + '/brewlog.bsmx', 'Recipe');
 	});
 });
 
