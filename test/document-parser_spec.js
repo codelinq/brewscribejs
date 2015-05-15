2c7755713ccac40b0fcb405fbca37031d67356ee
Scott Beardsley <scott@beards.ly>
2015-05-14 22:15:27 -0400

    document parser

---
+++ test/document-parser_spec.js
@@ -0,0 +1,56 @@
+var should = require('should');
+var DocumentParser = require('../lib/document-parser');
+
+describe('DocumentParser', function(){
+	var recipe_file = __dirname + '/brewlog.bsmx';
+	var parser = null;
+	
+	before(function(){
+		parser = new DocumentParser();
+	});
+	
+	describe('parse', function(){
+		
+		it('should parse a node when a node is found', function(done){
+			
+			var nodes = [];
+			
+			parser.on('node', function(node){
+				nodes.push(node);	
+			});
+		
+			parser.on('end', function(){
+				nodes.length.should.be.above(0);
+				done();
+			});
+		
+			parser.parse(recipe_file, 'Recipe');
+			
+		});
+		
+	});
+	
+	describe('clean_key', function(){
+		
+		it('converts an ugly key into something more readable', function(){
+			parser.clean_key('F_Y_BREW_DATE').should.eql('brew_date');
+		});
+		
+		it('converts _MOD_ key to last_modified', function(){
+			parser.clean_key('_MOD_').should.eql('last_modified');
+		});
+	});
+	
+	describe('xml_to_json', function(){
+		
+		it('converts xml to a corresponding JSON object', function(done){
+			var xml = '<foo><bar><baz>1</baz></bar><bazaar>2</bazaar></foo>';
+			var expected = { foo : { bar: { baz: '1' }, bazaar: '2'} };
+			parser.xml_to_json(xml, function(err, actual){
+				actual.should.eql(expected);
+				done();
+			});
+		});
+		
+	});
+});
--- test/document_spec.js
+++ test/document_spec.js
@@ -1,24 +1,20 @@
 var should = require('should');
-var DocumentParser = require('./../lib/document-parser');
-/*
-describe('document_parser', function(){
-	it('should spit out lines', function(done){
-		this.timeout(7000);
-		var parser = new DocumentParser();
-		var nodes = [];
+var Document = require('../lib/document');
+
+describe('Document', function(){
+	var recipe_file = __dirname + '/brewlog.bsmx';
+	var subject = null;
+	
+	before(function(done){
+		subject = new Document();
+		subject.parse(recipe_file, done);
+	});
+	
+	describe('parse', function(){
 		
-		parser.on('node', function(node){
-			nodes.push(node);	
+		it('should add a Recipe to recipes when a Recipe entry is found', function(){
+			subject.recipes.length.should.be.above(0);
 		});
 		
-		parser.on('end', function(){
-			nodes.forEach(function(node){
-				console.log(node.recipe.name + ' - ' + node.recipe.style.name);
-			});
-			done();
-		});
-		
-		parser.parse(__dirname + '/brewlog.bsmx', 'Recipe');
 	});
 });
-*/
