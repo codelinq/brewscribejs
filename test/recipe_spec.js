cd01c462f4995124a238872a16926e6560e830f4
Scott Beardsley <scott@beards.ly>
2016-02-17 00:26:22 -0500

    fixed bug in hop parsing

--- test/recipe_spec.js
+++ test/recipe_spec.js
@@ -17,7 +17,7 @@ var recipe_file = __dirname + '/brewlog.bsmx';
 describe('Recipe', function(){
 	var recipe = null;
 	var document = null;
-	
+
 	before(function(done){
 		document = new Document();
 		document.parse(recipe_file, function(){
@@ -25,31 +25,31 @@ describe('Recipe', function(){
 			done();
 		});
 	});
-	
+
 	it('should convert the recipe data into properties', function(){
 		recipe.brewer.should.eql('Scott');
 	});
-	
+
 	it('should convert equipment to an Equipment object', function(){
 		recipe.equipment.should.be.instanceOf(Equipment);
 	});
-	
+
 	it('should convert mash into a Mash object', function(){
 		recipe.mash.should.be.instanceOf(Mash);
 	});
-	
+
 	it('should convert style into a Style object', function(){
 		recipe.style.should.be.instanceOf(Style);
 	});
-	
+
 	it('should convert carb into a Carbonation object', function(){
 		recipe.carb.should.be.instanceOf(Carbonation);
 	});
-	
+
 	it('should convert ingredients to an IngredientList', function(){
 		recipe.ingredients.should.be.instanceOf(IngredientList);
 	});
-	
+
 	it('should set floats to numbers and dates to dates', function(){
 		recipe.boil_vol_measured.should.be.Number;
 		recipe.carb_vols.should.be.Number;
@@ -74,40 +74,40 @@ describe('Recipe', function(){
 		recipe.inv_date.should.be.Date;
 		recipe.last_modified.should.be.Date;
 	});
-	
+
 	it('should set the type to one of the TYPES', function(){
 		recipe.types.should.containEql(recipe.type);
 	});
-	
+
 	it('should set include_starter to boolean', function(){
 		[true, false].should.containEql(recipe.include_starter);
 	});
-	
+
 	it('should set locked to boolean', function(){
 		[true, false].should.containEql(recipe.locked);
 	});
-	
+
 	it('should set rebalance_scale to boolean', function(){
 		[true, false].should.containEql(recipe.rebalance_scale);
 	});
-	
+
 	it('should set stir_plate to boolean', function(){
 		[true, false].should.containEql(recipe.stir_plate);
 	});
-	
+
 	describe('calculation method', function(){
 		it('og_measured should be 1.062', function(){
 			recipe.og_measured.should.be.eql(1.062);
 		});
-		
+
 		it('fg_measured should be 1.004', function(){
 			recipe.fg_measured.should.be.eql(1.004);
 		});
-		
+
 		it('abv should calculate to 7.68', function(){
 			recipe.abv.should.be.eql(7.68);
 		});
-		
+
 		it('second recipe should not contain the same getter values', function(){
 			recipe.name.should.not.eql(document.recipes[1].name);
 			recipe.og_measured.should.not.be.eql(document.recipes[1].og_measured);
@@ -115,15 +115,15 @@ describe('Recipe', function(){
 			recipe.abv.should.not.be.eql(document.recipes[1].abv);
 		});
 	});
-	
+
 	it('ibu should be 66.4', function(){
 		recipe.ibu.should.be.eql(66.4);
 	});
-	
+
 	it('srm should be 6.1', function(){
 		recipe.srm.should.be.eql(6.1);
 	});
-	
+
 	it('estimated original gravity should be 1.074', function(){
 		recipe.original_gravity.should.be.eql(1.074);
 	});
