05d8b7037501ddaccc12794f319ab17cc16bcc95
Scott Beardsley <scott@beards.ly>
2017-01-31 17:41:04 -0500

    added stream buffer parsing

--- test/document-parser_spec.js
+++ test/document-parser_spec.js
@@ -1,48 +1,50 @@
 var should = require('should');
 var DocumentParser = require('../lib/document-parser');
+var fs = require('fs');
 
 describe('DocumentParser', function(){
 	var recipe_file = __dirname + '/brewlog.bsmx';
 	var parser = null;
-	
+
 	before(function(){
 		parser = new DocumentParser();
 	});
-	
+
 	describe('parse', function(){
-		
+
 		it('should parse a node when a node is found', function(done){
-			
+
 			var nodes = [];
-			
+
 			parser.on('node', function(node){
-				nodes.push(node);	
+				nodes.push(node);
 			});
-		
+
 			parser.on('end', function(){
 				nodes.length.should.be.above(0);
 				done();
 			});
-		
-			parser.parse(recipe_file, 'Recipe');
-			
+
+			var buf = fs.readFileSync(recipe_file);
+			parser.parse(buf, 'Recipe');
+
 		});
-		
+
 	});
-	
+
 	describe('clean_key', function(){
-		
+
 		it('converts an ugly key into something more readable', function(){
 			parser.clean_key('F_Y_BREW_DATE').should.eql('brew_date');
 		});
-		
+
 		it('converts _MOD_ key to last_modified', function(){
 			parser.clean_key('_MOD_').should.eql('last_modified');
 		});
 	});
-	
+
 	describe('xml_to_json', function(){
-		
+
 		it('converts xml to a corresponding JSON object', function(done){
 			var xml = '<foo><bar><baz>1</baz></bar><bazaar>2</bazaar></foo>';
 			var expected = { foo : { bar: { baz: '1' }, bazaar: '2'} };
@@ -51,6 +53,6 @@ describe('DocumentParser', function(){
 				done();
 			});
 		});
-		
+
 	});
 });
